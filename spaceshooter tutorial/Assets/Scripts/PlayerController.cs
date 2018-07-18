1f9aa2075b5e7e33f349511edde20154f91a7266
Jerzy@PC <jerzy.dabrowka@o2.pl>
2018-07-18 12:49:56 +0200

    shooting shots

--- spaceshooter tutorial/Assets/Scripts/PlayerController.cs
+++ spaceshooter tutorial/Assets/Scripts/PlayerController.cs
@@ -16,6 +16,21 @@ public class PlayerController : MonoBehaviour
 	public float tilt;
 	public Boundary boundary;
 
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate = 0.5f;
+	private float nextFire = 0.0f;
+
+	void Update ()
+	{
+		if (Input.GetButton("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+		
+		//GameObject clone = 
+		Instantiate (shot, shotSpawn.position, shotSpawn.rotation); // as GameObject;
+		}
+	}
 
 	void FixedUpdate ()
 	{	
