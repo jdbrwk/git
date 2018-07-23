b868e2c1d179fbec89f4861afc59008afd2fcc2c
Jerzy@Mac <jerzy.dabrowka@o2.pl>
2018-07-23 10:18:01 +0200

    creating asteroid, destroying asteroid on collision with bolt and not with boundary

---
+++ spaceshooter tutorial/Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Boundary")
+        {
+            return;
+        }
+
+        Debug.Log(other.name);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
