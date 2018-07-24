1e501836d800c51ac9dfd1c9c4e7332ac8b2d966
Jerzy@Mac <jerzy.dabrowka@o2.pl>
2018-07-24 22:39:53 +0200

    spawning asteroid

--- spaceshooter tutorial/Assets/Scripts/DestroyByContact.cs
+++ spaceshooter tutorial/Assets/Scripts/DestroyByContact.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DestroyByContact : MonoBehaviour {
 
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
@@ -11,7 +14,12 @@ public class DestroyByContact : MonoBehaviour {
             return;
         }
 
-        Debug.Log(other.name);
+        Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+        }
+
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
